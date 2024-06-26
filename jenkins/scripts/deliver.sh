c747d0b73b83ccc43a44c8453e66a8ea8958a126
TetianaDmytrash <101727759+TetianaDmytrash@users.noreply.github.com>
2024-06-26 11:39:19 +0300

    new way to target

--- jenkins/scripts/deliver.sh
+++ jenkins/scripts/deliver.sh
@@ -1,32 +1,34 @@
 #!/usr/bin/env bash
+#!/bin/bash
 
 echo 'The following Maven command installs your Maven-built Java application'
 echo 'into the local Maven repository, which will ultimately be stored in'
 echo 'Jenkins''s local Maven repository (and the "maven-repository" Docker data'
 echo 'volume).'
 set -x
-mvn jar:jar install:install help:evaluate -Dexpression=project.name
+mvn clean package
 set +x
 
 echo 'The following command extracts the value of the <name/> element'
 echo 'within <project/> of your Java/Maven project''s "pom.xml" file.'
 set -x
-NAME=`mvn -q -DforceStdout help:evaluate -Dexpression=project.name`
+NAME=$(mvn -q -DforceStdout help:evaluate -Dexpression=project.name)
 set +x
+echo "Project name: $NAME"
 
 echo 'The following command behaves similarly to the previous one but'
 echo 'extracts the value of the <version/> element within <project/> instead.'
 set -x
-VERSION=`mvn -q -DforceStdout help:evaluate -Dexpression=project.version`
+VERSION=$(mvn -q -DforceStdout help:evaluate -Dexpression=project.version)
 set +x
+echo "Project version: $VERSION"
 
 echo 'The following command runs and outputs the execution of your Java'
 echo 'application (which Jenkins built using Maven) to the Jenkins UI.'
 set -x
-# java -jar target/${NAME}-${VERSION}.jar
 
 # Проверка существования JAR файла
-JAR_FILE="my-app/1.0-SNAPSHOT/${NAME}-${VERSION}.jar"
+JAR_FILE="target/${NAME}-${VERSION}.jar"
 if [[ -f "$JAR_FILE" ]]; then
     java -jar "$JAR_FILE"
 else
