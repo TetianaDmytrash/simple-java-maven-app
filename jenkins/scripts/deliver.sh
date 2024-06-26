3eb6fe0cce45d0683e69db8c8cde50d14bd1e999
TetianaDmytrash <101727759+TetianaDmytrash@users.noreply.github.com>
2024-06-26 15:39:57 +0300

    add correct pwd

--- jenkins/scripts/deliver.sh
+++ jenkins/scripts/deliver.sh
@@ -1,6 +1,10 @@
 #!/usr/bin/env bash
 #!/bin/bash
 
+# Вывод текущего пути
+echo "Current working directory:"
+pwd
+
 echo 'The following Maven command installs your Maven-built Java application'
 echo 'into the local Maven repository, which will ultimately be stored in'
 echo 'Jenkins''s local Maven repository (and the "maven-repository" Docker data'
@@ -8,6 +12,10 @@ echo 'volume).'
 set -x
 mvn clean package
 set +x
+
+# Проверка содержимого директории target после сборки
+echo 'Contents of target directory after build:'
+ls -l target
 
 echo 'The following command extracts the value of the <name/> element'
 echo 'within <project/> of your Java/Maven project''s "pom.xml" file.'
@@ -27,10 +35,11 @@ echo 'The following command runs and outputs the execution of your Java'
 echo 'application (which Jenkins built using Maven) to the Jenkins UI.'
 set -x
 
-echo 'pwd'
 # Проверка существования JAR файла
-JAR_FILE="/var/jenkins_home/workspace/github_pipeline_2024_06/target/${NAME}-${VERSION}.jar"
+JAR_FILE="target/${NAME}-${VERSION}.jar"
 if [[ -f "$JAR_FILE" ]]; then
+    echo "JAR file $JAR_FILE found. Checking permissions:"
+    ls -l "$JAR_FILE"
     java -jar "$JAR_FILE"
 else
     echo "Error: JAR file $JAR_FILE not found."
